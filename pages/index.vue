199a0f40083608b170cd9f54d92f4d360ee8854b
idrisAkbarA <idrissmp32@gmail.com>
2020-05-17 10:19:02 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-container fluid fill-height>
+      <v-card>
+        <v-card-title>
+          Hey! Blog Himatif Lagi dibuat lo!!!
+        </v-card-title>
+        <v-card-text>
+          Semangat!!!
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
